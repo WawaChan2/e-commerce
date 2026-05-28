72538abc7dcbb8d0766e464d0da2adb5c0ed93c9
f <fwraah22@gmail.com>
2026-05-28 12:23:37 +0800

    Order dashboard, styles not update yet

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,5 +1,50 @@
-<script setup></script>
+<script setup>
+import { useProductsStore } from '@/stores/products.js';
+import { onMounted } from "vue";
 
-<template></template>
+const productsStore = useProductsStore();
 
-<style scoped></style>
+onMounted(() => {
+    productsStore.fetchOrders();
+});
+</script>
+
+<template>
+    <h2>Order Management Dashboard</h2>
+    <div class="container">
+        <table border="1" cellpadding="10" style="border-collapse:collapse; width:100%; ">
+            <thead>
+                <tr>
+                    <th>Order ID</th>
+                    <th>User ID</th>
+                    <th>Order Date</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="order in productsStore.orders" :key="order.order_id">
+                    <td>#{{ order.order_id }}</td>
+                    <td>User {{ order.user_id }}</td>
+                    <td>{{ order.order_date }}</td>
+                    <td>{{ order.status }}</td>
+                    <td>
+                        <button
+                          v-if="order.status !== 'CANCELLED'"
+                          @click="productsStore.cancelOrder(order.order_id)"
+                        >
+                          Cancel Order (Rollback Transaction)
+                    </button>
+                    <span v-else>No Actions</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    padding: 20px;
+}
+</style>
